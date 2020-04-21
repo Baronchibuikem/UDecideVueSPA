35c615bcfedd97a98ce6299f59502c11d890f039
baronchibuikem <baronchibuike@gmail.com>
2020-04-21 01:39:58 +0100

    deploying on heroku

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
+const port = process.env.PORT || 8080;
+// const serveStatic = require("serve-static");
+// const path = require("path");
 
 const app = express();
 
 // serves files from our dist directory which now contains our index.html
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+// app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+	res.sendFile(__dirname + "/dist/index.html");
+});
 
-const port = process.env.port || 8080;
 app.listen(port);
 console.log("listening on port: " + port);
